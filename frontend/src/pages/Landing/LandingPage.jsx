194bbd1c6c919e204e3ae4037cffd35b3cc44a6c
Pedro Sousa <145287309+PedroSousa1602@users.noreply.github.com>
2026-01-12 13:18:08 +0000

    enhance landing page design and animations

--- frontend/src/pages/Landing/LandingPage.jsx
+++ frontend/src/pages/Landing/LandingPage.jsx
@@ -7,28 +7,24 @@ export default LandingPage;
 
 function LandingPage() {
     return (
-        <div className="landing-Page">
-            
-            <div className="landing-header">
-                <header>
-                    <h1>Dear Me</h1>
-                        <div className="Auth">
-                            <Link to="/auth" className="login-button">Entrar / Cadastrar</Link>
-                        </div>
-                </header> 
-            </div>
+      <div className="landing-Page">
+    <div className="landing-header">
+        <header>
+            <h1>Dear Me</h1>
+        </header> 
+    </div>
 
-            <div className="container">
-                <div className="conth2">
-                    <h2>Bem Vindo ao Dear Me</h2>
-                    <p>Seu espaço pessoal para refletir, crescer e se conectar com seu eu interior. Comece sua jornada hoje!</p>
-                </div>
-                
-                <div className="contImage">
-                    <img src={imagemBoneco} alt="Boneco" />
-                </div>
-            </div>
-
+    <div className="container">
+        <div className="conth2">
+            <h2>Bem Vindo ao Dear Me</h2>
+            <p>Seu espaço pessoal para refletir, crescer e se conectar com seu eu interior. Comece sua jornada hoje!</p>
+            <Link to="/auth" className="authbutton">Começar Jornada</Link>
         </div>
+        
+        <div className="contImage">
+            <img src={imagemBoneco} alt="Boneco" />
+        </div>
+    </div>
+</div>
     );
 }
