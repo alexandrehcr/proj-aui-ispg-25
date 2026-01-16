ade1e16bb59451212e5c0fb375cba5312107a798
PedroSousa1602 <pedrocgs2006@gmail.com>
2026-01-16 23:53:47 +0000

    Added createPost function and updated styles for modal form

--- frontend/src/pages/Feed/DearMe.jsx
+++ frontend/src/pages/Feed/DearMe.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from 'axios';
 import "./style.css";
 
 export default DearMe;
@@ -6,9 +7,33 @@ export default DearMe;
 function DearMe() {
 
     const [modalAberto, setModalAberto] = useState(false);
-
     const abrirModal = () => setModalAberto(true);
     const fecharModal = () => setModalAberto(false);
+
+    function createPost(FormData) {
+
+        const tittle = FormData.get("tittle");
+        const content = FormData.get("content");
+        const coverB64 = FormData.get("coverB64");
+
+        const postData = ({
+            titulo: tittle,
+            content: content,
+            coverB64: coverB64
+        });
+        console.log(postData);
+
+        try {
+            const response = axios.post("http://localhost:5000/createpost", postData);
+            console.log("Publicação criada com sucesso:", response.data);
+            alert("Publicação criada com sucesso!");    
+        } catch (error) {
+            console.error("Erro ao criar publicação:", error.response?.data || error.message);
+            alert("Erro ao criar publicação: " + (error.response?.data?.message || "Servidor offline"));
+        }
+
+        fecharModal();
+    }
 
 
     return (
@@ -20,18 +45,18 @@ function DearMe() {
             </div>
             <div className="create-post">
                 <button onClick={abrirModal}>Criar Publicação</button>
-            </div>
+        </div>
 
             {modalAberto && (
             <>
             <div className="overlay" onClick={fecharModal}></div>
+            
             <div className="formPost">
-                
-                <h2>Formulário de Publicação</h2>
-                    <form className="modal-form">
-                        <input type="text" placeholder="Título" />
-                        <textarea placeholder="Escreva sua publicação..."></textarea>
-                        <input type="file" />
+                <h2>Criar Publicacao</h2>
+                    <form className="modal-form" action={createPost} >
+                        <input name="tittle" type="text" placeholder="Título" required/>                
+                        <textarea name="content" className="content" placeholder="Escreva sua publicação..." required></textarea>
+                        <input name="coverB64" className="coverB64" type="file" />
                         <button type="submit">Publicar</button>
                     </form>
                 <button onClick={fecharModal}>X</button>
